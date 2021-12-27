99a222c2f3daa6825c0d556472867cd6c354ca09
shilpaspt <96714765+shilpaspt@users.noreply.github.com>
2021-12-27 11:44:55 +0530

    Create Shilpa.C

---
+++ Shilpa.C
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @shilpaspt
+- 👀 I’m interested in gaining knowledge
+- 🌱 I’m currently learning python and machince learning
+- 💞️ I’m looking to collaborate on nueral networks
+- 📫 How to reach me Shilpasiet@gmail.com
+
+<!---
+shilpaspt/shilpaspt is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
