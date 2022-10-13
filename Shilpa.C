bf77508eb0b8cef56212e2982a314851ab1d5ed1
sandeep6421 <107025652+sandeep6421@users.noreply.github.com>
2022-10-13 16:10:14 +0530

    Update Shilpa.C

--- Shilpa.C
+++ Shilpa.C
@@ -4,7 +4,7 @@
 - 💞️ I’m looking to collaborate on nueral networks
 - 📫 How to reach me Shilpasiet@gmail.com
 
-<!---
+<!----
 shilpaspt/shilpaspt is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
---->
+---->
